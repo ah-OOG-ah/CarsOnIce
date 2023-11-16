482d2844052c2d02d7dd3f1b4ddf6407b7e648b9
SunGillete <151089908+SunGillete@users.noreply.github.com>
2023-11-16 17:17:13 -0500

    testing

--- cars_on_ice.m
+++ cars_on_ice.m
@@ -1,9 +1,10 @@
 % A calculator for banked roads
 clear
 clc
-pkg load symbolic
+%pkg load symbolic
 
-arrayfun(find_bank, 100:200)
+%plot(100:20:3000, rad2deg(arrayfun(@find_bank, 100:20:3000)))
+plot(100:20:3000, rad2deg(find_bank(100:20:3000)))
 
 
 
--- find_bank.m
+++ find_bank.m
@@ -3,10 +3,11 @@ function ret = find_bank(radius)
   g = 9.8;
   us = 0.7;
   R = radius;
-  theta = deg2rad(0:20);
-  vmax = 40;
-  eq = vmax==sqrt(g * R * (sin(theta) + us * cos(theta)) / (cos(theta) - us * sin(theta)));
+  theta = deg2rad(2);
+  %vmax = 40;
+  syms vmax
+  eq = vmax==sqrt(g * R * (tan(theta) + us) / (1 - us * tan(theta)));
 
 
-  ret = vpasolve(eq, theta);
+  ret = vpasolve(eq, vmax);
 end
