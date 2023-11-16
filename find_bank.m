4edc55b3efb90abcc14b18384fedfb6baccab062
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2023-11-16 16:47:08 -0500

    Update find_bank.m

--- find_bank.m
+++ find_bank.m
@@ -3,7 +3,7 @@ function ret = find_bank(radius)
   g = 9.8;
   us = 0.7;
   R = radius;
-  %theta = deg2rad(0:20);
+  theta = deg2rad(0:20);
   vmax = 40;
   eq = vmax==sqrt(g * R * (sin(theta) + us * cos(theta)) / (cos(theta) - us * sin(theta)));
 
